e85c622260c65b8a92ccee401a7fa6ca26647665
ikemesit <ikemesit@gmail.com>
2016-08-24 11:55:22 +0800

    PARTIAL UPDATES

--- src/app/deals/controllers/deal.controller.js
+++ src/app/deals/controllers/deal.controller.js
@@ -1,45 +1,45 @@
 (function(){
-	'use strict';
+  'use strict';
 
-	angular
-		.module('xandha')
-		.controller('DealController', DealController);
-
-
-	function DealController($uibModal, $log, $stateParams){
-		var vm = this;
-			vm.deal = null;
-			vm.selectedDeal = null;
-			vm.openModal = openModal;
+  angular
+    .module('xandha')
+    .controller('DealController', DealController);
 
 
-		$log.info($stateParams.key);
-		function openModal(){
-			var modalInstance = $uibModal.open({
-				ariaLabelledBy: 'modal-title',
-				ariaDescribedBy: 'modal-body',
-				// backdropClass: 'custom-modal',
-				windowClass: 'modal fadeInDown in',
-				templateUrl: 'app/deals/templates/dealsModal.html',
-				controller: 'ModalInstanceController',
-				controllerAs: '$ctrl',
-				size: 'lg',
-				resolve: {
-					deal: function () {
-					  return vm.selectedDeal;
-					}
-				}
-			});
-
-			modalInstance.result.then(function () {
-				// $ctrl.selected = selectedItem;
-			}, function () {
-				$log.info('Modal dismissed at: ' + new Date());
-			});
-		}
-			
+  function DealController($uibModal, $log, $stateParams){
+    var vm = this;
+      vm.deal = null;
+      vm.selectedDeal = null;
+      vm.openModal = openModal;
 
 
+    $log.info($stateParams.key);
+    function openModal(){
+      var modalInstance = $uibModal.open({
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        // backdropClass: 'custom-modal',
+        windowClass: 'modal fadeInDown in',
+        templateUrl: 'app/deals/templates/dealsModal.html',
+        controller: 'ModalInstanceController',
+        controllerAs: '$ctrl',
+        size: 'lg',
+        resolve: {
+          deal: function () {
+            return vm.selectedDeal;
+          }
+        }
+      });
 
-	}// End
+      modalInstance.result.then(function () {
+        // $ctrl.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    }
+
+
+
+
+  }// End
 })();
